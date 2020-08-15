6daa5ff51f05d91f33091448f5264f1af9e52a50
Ian Meinert <ian.meinert@gmail.com>
2020-08-14 22:18:01 -0400

    fox_totem recipe and tooltip

--- src/main/java/net/foxesden/common/item/FoxTotemItem.java
+++ src/main/java/net/foxesden/common/item/FoxTotemItem.java
@@ -1,12 +1,18 @@
 package net.foxesden.common.item;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.world.World;
+
+import java.util.List;
 
 public class FoxTotemItem extends Item {
 
@@ -30,5 +36,10 @@ public class FoxTotemItem extends Item {
         this.use(user.world, user, hand);
 
         return ActionResult.PASS;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(new TranslatableText("item.foxesden.fox_totem.tooltip"));
     }
 }
